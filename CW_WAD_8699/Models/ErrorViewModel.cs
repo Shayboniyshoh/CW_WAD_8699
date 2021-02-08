58e4a4baf148ec0faed89a4ee953e5fe3ea4fd7c
Shayboniyshoh <shayboniyshohabduvaliev@gmail.com>
2021-02-08 21:04:23 +0500

    Creating the empty project

---
+++ CW_WAD_8699/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CW_WAD_8699.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
