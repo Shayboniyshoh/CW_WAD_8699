1a9c2e788954c6780170e53a1b0e6a7a580226e6
Shayboniyshoh <shayboniyshohabduvaliev@gmail.com>
2021-03-12 20:32:27 +0500

    Calling: Database with connection string

--- CW_WAD_8699/Startup.cs
+++ CW_WAD_8699/Startup.cs
@@ -1,6 +1,8 @@
+using CW_WAD_8699.DAL;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +26,10 @@ namespace CW_WAD_8699
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<LibraryDataContext>(
+                    options => options.UseSqlServer(
+                        Configuration.GetConnectionString("CWDB8699"))
+                );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
