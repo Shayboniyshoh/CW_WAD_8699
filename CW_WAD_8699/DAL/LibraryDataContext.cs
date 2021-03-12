0c6c41c14c9dc0c4a4cc3a7ac4192288cbadd22f
Shayboniyshoh <shayboniyshohabduvaliev@gmail.com>
2021-03-12 20:24:38 +0500

    Adding: Data Context to the project

---
+++ CW_WAD_8699/DAL/LibraryDataContext.cs
@@ -0,0 +1,15 @@
+﻿using CW_WAD_8699.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CW_WAD_8699.DAL
+{
+    public class LibraryDataContext : DbContext
+    {
+        public LibraryDataContext(DbContextOptions<LibraryDataContext> options) : base(options)
+        {
+
+        }
+        public virtual DbSet<Book> Books { get; set; }
+        public DbSet<CW_WAD_8699.Models.Category> Categories { get; set; }
+    }
+}
