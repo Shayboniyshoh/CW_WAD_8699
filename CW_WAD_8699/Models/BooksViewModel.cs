750aced9db61f5fe4f2ed38b50365306610c3783
Shayboniyshoh <shayboniyshohabduvaliev@gmail.com>
2021-03-12 23:51:42 +0500

    Moved book and category models to the other project, and added BooksViewModel

---
+++ CW_WAD_8699/Models/BooksViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CW_WAD_8699.Models
+{
+    public class BooksViewModel : Book
+    {
+        public SelectList Categories { get; set; }
+    }
+}
