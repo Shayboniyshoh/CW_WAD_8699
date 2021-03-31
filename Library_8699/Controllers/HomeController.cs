7b8fe999150b5af56d1090c6dfa72469f3d7a63f
Shayboniyshoh <shayboniyshohabduvaliev@gmail.com>
2021-03-31 10:19:16 +0500

    removed unnecessrary method

--- Library_8699/Controllers/HomeController.cs
+++ Library_8699/Controllers/HomeController.cs
@@ -19,11 +19,6 @@ namespace Library_8699.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
